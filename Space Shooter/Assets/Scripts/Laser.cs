e44400ceebebbeba0c5822d659a5c86679079959
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-05-04 10:18:41 +0200

    Laser behaviour

---
+++ Space Shooter/Assets/Scripts/Laser.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    private int _speed = 8;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+    }
+}
