69d0c69909a9c3e17b2535fa74cd52d864c71460
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-05-05 10:48:56 +0200

    enemy behaviour

---
+++ Space Shooter/Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private float _speed = 4f;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+        if(transform.position.y < -5.35f)
+        {
+            transform.position = new Vector3(Random.Range(-11.62f, 11.65f), 6.92f, 0);
+        }
+    }
+}
