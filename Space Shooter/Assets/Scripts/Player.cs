e1ff7a25e96266b119bda832fcf0311b07aa1bdf
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-05-07 10:51:52 +0200

    laser prefab

--- Space Shooter/Assets/Scripts/Player.cs
+++ Space Shooter/Assets/Scripts/Player.cs
@@ -9,12 +9,16 @@ public class Player : MonoBehaviour
     [SerializeField]
     private GameObject _laserPrefab;
     [SerializeField]
+    private GameObject _trippleShotPrefab;
+    [SerializeField]
     private float _fireRate = 0.5f;
     private float _canFire = -1f;
     Vector3 offset = new Vector3(0, 1.05f, 0);
     [SerializeField]
     private int _lives = 3;
     private SpawnManager _spawnManager;
+    [SerializeField]
+    private bool _isTrippleShotActive = false;
 
     void Start()
     {
@@ -60,7 +64,16 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
         {
             _canFire = Time.time + _fireRate;
-            Instantiate(_laserPrefab, transform.position + offset, Quaternion.identity);
+
+            if (_isTrippleShotActive == true)
+            {
+                Instantiate(_trippleShotPrefab, transform.position, Quaternion.identity);
+            }
+            else
+            {
+                Instantiate(_laserPrefab, transform.position + offset, Quaternion.identity);
+            }
+            
         }
     }
 
