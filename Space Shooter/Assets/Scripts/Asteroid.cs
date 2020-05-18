050492ac4cb5195ef16d04008db67a2a4e099c9d
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-05-18 09:59:23 +0200

    Asteroid behaviour

---
+++ Space Shooter/Assets/Scripts/Asteroid.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    [SerializeField]
+    float _floatSpeed = 19f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * _floatSpeed * Time.deltaTime);
+    }
+
+}
