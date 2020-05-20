c2c2b97352572f35db8e9fa2fc918c5f6c454dd5
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-05-20 10:47:26 +0200

    Enemy Laser

--- Space Shooter/Assets/Scripts/Enemy.cs
+++ Space Shooter/Assets/Scripts/Enemy.cs
@@ -7,12 +7,15 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 4f;
+    [SerializeField]
+    private GameObject _laserPrefab;
 
     private Player _player;
     private Animator _anim;
     private BoxCollider2D _enemyCollider;
     private AudioSource _audioSource;
-
+    private float _fireRate = 3f;
+    private float _canFire = -1;
 
     void Start()
     {
@@ -36,6 +39,25 @@ public class Enemy : MonoBehaviour
 
     
     void Update()
+    {
+        CalculateMovement();
+        
+        if(Time.time > _canFire)
+        {
+            _fireRate = Random.Range(3f, 7f);
+            _canFire = Time.time + _fireRate;
+            GameObject enemyLaser = Instantiate(_laserPrefab, transform.position, Quaternion.identity);
+            Laser[] lasers = enemyLaser.GetComponentInChildren<Laser[]>();
+            
+            for(int i = 0; i < lasers.Length; i++)
+            {
+                lasers[i].AssignEnemyLaser();
+            }
+
+        }
+    }
+
+    void CalculateMovement()
     {
         transform.Translate(Vector3.down * _speed * Time.deltaTime);
 
--- Space Shooter/Assets/Scripts/Laser.cs
+++ Space Shooter/Assets/Scripts/Laser.cs
@@ -1,23 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.XR;
 using UnityEngine;
 
 public class Laser : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 8f;
+    private bool _isEnemyLaser = false;
+
    
     void Update()
     {
+        if(_isEnemyLaser == false)
+        {
+            MoveUp();
+        }
+        else
+        {
+            MoveDown();
+        }
+
+    }
+
+    void MoveUp()
+    {
         transform.Translate(Vector3.up * _speed * Time.deltaTime);
 
-        if(transform.position.y > 8f)
+        if (transform.position.y > 8f)
         {
-            if(transform.parent != null)
+            if (transform.parent != null)
             {
                 Destroy(transform.parent.gameObject);
             }
             Destroy(gameObject);
         }
     }
+
+    void MoveDown()
+    {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+        if (transform.position.y > -8f)
+        {
+            if (transform.parent != null)
+            {
+                Destroy(transform.parent.gameObject);
+            }
+            Destroy(gameObject);
+        }
+    }
+
+    public void AssignEnemyLaser()
+    {
+        _isEnemyLaser = true;
+    }
+
 }
