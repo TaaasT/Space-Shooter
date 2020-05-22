37ec3fac69ae7316818ce4b5107e3a118dea43ff
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-05-22 12:41:54 +0200

    Enemy fire Fix

--- Space Shooter/Assets/Scripts/Enemy.cs
+++ Space Shooter/Assets/Scripts/Enemy.cs
@@ -45,15 +45,21 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         CalculateMovement();
-        
-        if(Time.time > _canFire)
+        EnemyFire();
+    }
+
+    private void EnemyFire()
+    {
+        if (_speed == 0) return;
+
+        if (Time.time > _canFire)
         {
             _fireRate = Random.Range(3f, 7f);
             _canFire = Time.time + _fireRate;
             GameObject enemyLaser = Instantiate(_laserPrefab, transform.position, Quaternion.identity);
             Laser[] lasers = enemyLaser.GetComponentsInChildren<Laser>();
-            
-            for(int i = 0; i < lasers.Length; i++)
+
+            for (int i = 0; i < lasers.Length; i++)
             {
                 lasers[i].AssignEnemyLaser();
             }
--- Space Shooter/Assets/Scripts/GameManager.cs
+++ Space Shooter/Assets/Scripts/GameManager.cs
@@ -13,7 +13,7 @@ public class GameManager : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.R) && _isGameOver == true)
         {
-            SceneManager.LoadScene(0); // Current Game Scene
+            SceneManager.LoadScene(0); // Main Menu
         }
 
         if(Input.GetKeyDown(KeyCode.Escape))
